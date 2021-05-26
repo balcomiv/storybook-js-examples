b7f5a6df908bf7a02d48e101986687f85d54c9cb
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-05-26 06:28:06 -0400

    udpate - app stories && readme

--- README.md
+++ README.md
@@ -26,6 +26,12 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
 
+## Load assets and resources
+
+If you want to link to static files in your project or stories (e.g. /fonts/XYZ.woff), use the -s path/to/folder to specify a static folder to serve from when you start up Storybook. To do so, edit the storybook and build-storybook scripts in package.json.
+
+We recommend serving external resources and assets requested in your components statically with Storybook. This ensures that assets are always available to your stories.
+
 # Additional Packages
 
 ## Storybook
--- src/app/app.stories.ts
+++ src/app/app.stories.ts
@@ -3,6 +3,7 @@ import { moduleMetadata } from '@storybook/angular';
 import { Meta, Story } from '@storybook/angular/types-6-0';
 import { AppComponent } from './app.component';
 
+// 👇 This default export determines where your story goes in the story list
 export default {
   title: 'Test/It',
   component: AppComponent,
@@ -11,12 +12,38 @@ export default {
       declarations: [AppComponent],
       imports: [RouterTestingModule],
     }),
+    // With template
+    // componentWrapperDecorator(
+    //   (story) =>
+    //     `<div style="border: 1px solid green; height: 200px;">${story}</div>`
+    // ),
+    // With component which contains ng-content
+    // componentWrapperDecorator(AppComponent),
   ],
 } as Meta;
 
+// 👇 We create a “template” of how args map to rendering
 const Template: Story<AppComponent> = (args: AppComponent) => ({
   props: args,
 });
 
-export const Primary = Template.bind({});
-Primary.args = {};
+export const withTemplateBind = Template.bind({});
+
+withTemplateBind.args = {
+  /* 👇 The args you need here will depend on your component */
+};
+
+// export const withCustomTemplate: Story<AppComponent> = (args) => ({
+//   template: '<app-root></app-root>',
+// });
+
+//  Also
+//
+// const Template: Story<AppComponent> = (args) => ({
+//   props: args,
+//   template: '<app-root [title]="title"></app-root>',
+// });
+
+// const Component: Story<AppComponent> = (args) => ({
+//   props: args,
+// });
